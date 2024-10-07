07b1df155c131a70b8231c1939e4e4b8f6bcfc5d
Marius-courquin <marius.courquin@ig2i.centralelille.fr>
2024-10-07 08:16:17 +0200

    feat(article): add orderId field

--- src/main/java/com/ig2i/instances/models/Article.java
+++ src/main/java/com/ig2i/instances/models/Article.java
@@ -2,14 +2,21 @@ package com.ig2i.instances.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class Article {
-    private int quantity;
-    private Product product;
+    private final int quantity;
+    private final Product product;
+    private int orderId;
 
     public static Article of(int quantity, Product product) {
         return new Article(quantity, product);
+    }
+
+    public static Article of(int quantity, Product product, int orderId) {
+        return new Article(quantity, product, orderId);
     }
 }
